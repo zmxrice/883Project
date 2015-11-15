f04be7de4f3a8d22389a5fa84ae84077082bf194
Mingxiang Zhu <mxiangzhu@gmail.com>
2015-11-15 16:40:57 -0500

    adding file for Delta features calculation

---
+++ getSequenceFeatures.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import math
+
+def CommonNeighbors(u, v, g):
+    u_neighbors = set(g.neighbors(u))
+    v_neighbors = set(g.neighbors(v))
+    return len(u_neighbors.intersection(v_neighbors))
+
+def AdamicAdar(u, v, g):
+    u_neighbors = set(g.neighbors(u))
+    v_neighbors = set(g.neighbors(v))
+    aa = 0
+    for i in u_neighbors.intersection(v_neighbors):
+        aa += 1 / math.log(len(g.neighbors(i)))
+    return aa
+
+def ResourceAllocation(u, v, g):
+    u_neighbors = set(g.neighbors(u))
+    v_neighbors = set(g.neighbors(v))
+    ra = 0
+    for i in u_neighbors.intersection(v_neighbors):
+        ra += 1 / float(len(g.neighbors(i)))
+    return ra
+
+def JaccardCoefficent(u, v, g):
+    u_neighbors = set(g.neighbors(u))
+    v_neighbors = set(g.neighbors(v))
+    return len(u_neighbors.intersection(v_neighbors)) / float(len(u_neighbors.union(v_neighbors)))
+
+def PreferentialAttachment(u, v, g):
+    return len(g.neighbors(u))*len(g.neighbors(v))
+
+def DeltaFeatures(u,v,g1, g2):
+    '''
+    the change of features in two consecutive sub graphs
+    '''
+    delta_cn = CommonNeighbors(u, v, g2) - CommonNeighbors(u, v, g1)
+    delta_aa = AdamicAdar(u, v, g2) - AdamicAdar(u, v, g1)
+    delta_ra = ResourceAllocation(u, v, g2) - ResourceAllocation(u, v, g1)
+    delta_jc = JaccardCoefficent(u, v, g2) - JaccardCoefficent(u, v, g1)
+    delta_pa = PreferentialAttachment(u, v, g2) - PreferentialAttachment(u, v, g1)
+    return {"delta_cn": delta_cn, "delta_aa": delta_aa, "delta_ra": delta_ra, "delta_jc": delta_jc, "delta_pa": delta_pa}
